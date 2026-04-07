879a87c55a54f45b5fe5f32293358f7ed2ead72f
Gavrilchenko-Stepan <atom47228@gmail.com>
2026-04-07 09:59:28 +0300

    Задача 1 выполнена(опять)

--- TestProject/ClassLibrary/Sale.cs
+++ TestProject/ClassLibrary/Sale.cs
@@ -12,11 +12,12 @@ namespace ClassLibrary
         public int Price { get; set; }
         public int Quantity { get; set; }
 
-        //public Sale(string name, int price, int quantity)
-        //{
-        //    Name = name;
-        //    Price = price;
-        //    Quantity = quantity;
-        //}
+        public Sale(string name, int price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public Sale() { }
     }
 }
--- TestProject/TestProject/ImporterTest.cs
+++ TestProject/TestProject/ImporterTest.cs
@@ -12,11 +12,22 @@ namespace TestProject
             string path;
             List<Sale> sales = new List<Sale>();
             Importer imp = new Importer();
-            path = "../../Texts/t.txt";
+            path = "../../../../Texts/t.txt";
             sales = imp.Import(path);
             List<Sale> expected = new List<Sale>();
-            CollectionAssert.AreEqual(expected, sales);
-            
+            expected.Add(new Sale("t", 1, 1));
+            expected.Add(new Sale("p", 2, 2));
+            expected.Add(new Sale("y", 3, 3));
+            expected.Add(new Sale("i", 4, 4));
+            Assert.AreEqual(expected.Count, sales.Count);
+            for (int i = 0; i < sales.Count; i++)
+            {
+                //Assert.AreEqual(expected[i], sales[i]);
+                Assert.AreEqual(expected[i].Name, sales[i].Name);
+                Assert.AreEqual(expected[i].Price, sales[i].Price);
+                Assert.AreEqual(expected[i].Quantity, sales[i].Quantity);
+            }
+
         }
     }
 }
